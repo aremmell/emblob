1fa36cdebacd2996adfa389cef12e1c3771d1633
Ryan M. Lederman <lederman@gmail.com>
2023-05-18 09:25:32 -0600

    attempt to resolve missing errno_t on travis linux

--- platform.hh
+++ platform.hh
@@ -5,12 +5,14 @@
 #define STR_MACRO(m) _STR_MACRO(m)
 
 #if defined(__APPLE__)
+#   define __MACOS__
 #   define MKVEROBJ_PLATFORM macOS
 #elif defined(__linux__)
 #   define MKVEROBJ_PLATFORM Linux
 #elif defined(_WIN32)
 #   define MKVEROBJ_PLATFORM Windows
-#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+#   define __BSD__
 #   define MKVEROBJ_PLATFORM BSD
 #else
 #   error "Unable to determine platform; please contact the author."
@@ -22,22 +24,23 @@
 #   define __WANT_STDC_SECURE_LIB__ 1
 #endif
 
+#include <sys/types.h>
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 #include <string>
 
 #if !defined(_WIN32) && defined(__STDC_LIB_EXT1__)
-#   define __HAVE_STDC_SECURE_LIB__
+#   define __HAVE_STDC_SECURE_OR_EXT1__
 #elif defined(__STDC_SECURE_LIB__)
-#   define __HAVE_STDC_SECURE_LIB__
+#   define __HAVE_STDC_SECURE_OR_EXT1__
 #endif
 
-#if defined(__MACOS__) || defined(__FreeBSD__) || (_POSIX_C_SOURCE >= 200112L && !defined(_GNU_SOURCE))
+#if defined(__MACOS__) || defined(__BSD__) || ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE)
 #   define __HAVE_XSI_STRERROR_R__
 #elif defined(_GNU_SOURCE)
 #   define __HAVE_GNU_STRERROR_R__
-#elif defined(__HAVE_STDC_SECURE_LIB__)
+#elif defined(__HAVE_STDC_SECURE_OR_EXT1__)
 #   define __HAVE_STRERROR_S__
 #   define __HAVE_FOPEN_S__
 #endif
@@ -59,13 +62,13 @@ namespace mkverobj
             bool success = true;
             int finderr = strerror_r(err, buf, MAX_ERRORMSG);
 
-#if defined(__GLIBC__) && (__GLIBC__ >= 2 && __GLIBC_MINOR < 13)
+#if defined(__GLIBC__) && (__GLIBC__ >= 2 && __GLIBC_MINOR__ < 13)
             if (finderr == -1) {
                 success = false;
                 finderr = errno;
             }
 #else
-                success = finderr == 0;
+            success = finderr == 0;
 #endif
             if (!success)
                 snprintf(buf, MAX_ERRORMSG, "Got error %d while trying to look up error %d", finderr, err);
