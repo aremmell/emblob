2a4de6a4b0d00c34df284a0efb8ee48b13a6d570
Ryan M. Lederman <lederman@gmail.com>
2023-06-08 04:23:54 -0600

    update filesystem functionality

--- support/systest.c
+++ support/systest.c
@@ -78,50 +78,82 @@ bool check_filesystem_api(void) {
     bool all_passed = true;
 
     /* ==== get app file name path (absolute path of binary file) ==== */
-    char tmp_path[SYSTEST_MAXPATH] = {0};
-    all_passed &= systest_getappfilename(tmp_path, SYSTEST_MAXPATH);
-    printf(WHITE("systest_getappfilename() = '%s'") "\n", tmp_path);
-    /* ==== */
+    char* appfilename = systest_getappfilename();
+    all_passed &= _validptr(appfilename);
+    if (_validptr(appfilename)) {
+        printf(WHITE("systest_getappfilename() = '%s'") "\n", appfilename);
+        /* ==== */
 
-    /* ==== get base name (file name component of a path)  ==== */
-    char* basenametest = strdup(tmp_path);
-    if (!basenametest) {
-        handle_error(errno, "strdup() failed!");
-        return false;
+        /* ==== get base name (file name component of a path)  ==== */
+        char* basenametest = strdup(appfilename);
+        if (!basenametest) {
+            handle_error(errno, "strdup() failed!");
+            return false;
+        }
+
+        char* basenameresult = systest_getbasename(basenametest);
+        all_passed &= (strlen(basenameresult) > 0 && 0 != strcmp(basenameresult, "."));
+        printf(WHITE("systest_getbasename() = '%s'") "\n", basenameresult);
+        systest_safefree(basenametest);
+        /* ==== */
+
+        /* ==== get dir name (directory structure component of a path)  ==== */
+        char* dirnametest = strdup(appfilename);
+        if (!dirnametest) {
+            handle_error(errno, "strdup() failed!");
+            return false;
+        }
+
+        char* dirnameresult = systest_getdirname(dirnametest);
+        all_passed &= (strlen(dirnameresult) > 0 && 0 != strcmp(dirnameresult, ".")); 
+        printf(WHITE("systest_getdirname() = '%s'") "\n", dirnameresult);
+        systest_safefree(dirnametest);
+        /* ==== */
+
+        systest_safefree(appfilename);
+    } else {
+        printf(RED("systest_getbasename() = skipped") "\n");
+        printf(RED("systest_getdirname() = skipped") "\n");
     }
-
-    char* basenameresult = systest_getbasename(basenametest);
-    all_passed &= (strlen(basenameresult) > 0 && 0 != strcmp(basenameresult, "."));
-    printf(WHITE("systest_getbasename() = '%s'") "\n", basenameresult);
-    free(basenametest);
-    basenametest = NULL;
-    /* ==== */
-
-    /* ==== get dir name (directory structure component of a path)  ==== */
-    char* dirnametest = strdup(tmp_path);
-    if (!dirnametest) {
-        handle_error(errno, "strdup() failed!");
-        return false;
-    }
-
-    char* dirnameresult = systest_getdirname(dirnametest);
-    all_passed &= (strlen(dirnameresult) > 0 && 0 != strcmp(dirnameresult, ".")); 
-    printf(WHITE("systest_getdirname() = '%s'") "\n", dirnameresult);
-    free(dirnametest);
-    dirnametest = NULL;    
-    /* ==== */
 
     /* ==== get app dir path (absolute path of directory containing binary file) ==== */
-    memset(tmp_path, '\0', SYSTEST_MAXPATH);    
-    all_passed &= systest_getappdir(tmp_path, SYSTEST_MAXPATH);
-    printf(WHITE("systest_getappdir() = '%s'") "\n", tmp_path);
+    char* appdir = systest_getappdir();
+    all_passed &= _validptr(appdir);
+    printf(WHITE("systest_getappdir() = '%s'") "\n", prn_str(appdir));
+    systest_safefree(appdir);
     /* ==== */
 
     /* ==== get current working directory (not necessarily the same ass app directory) ==== */
-    memset(tmp_path, '\0', SYSTEST_MAXPATH);    
-    all_passed &= systest_getcwd(tmp_path, SYSTEST_MAXPATH);
-    printf(WHITE("systest_getcwd() = '%s'") "\n", tmp_path);
+    char* cwd = systest_getcwd();
+    all_passed &= _validptr(cwd);
+    printf(WHITE("systest_getcwd() = '%s'") "\n", prn_str(cwd));
+    systest_safefree(cwd);
     /* ==== */
+
+    /* file existence: some we know exist, and some we know don't. */
+    static const struct { const char* const path; bool exists; } real_or_not[] = {
+        {"../../LICENSE", true},
+        {"./i_exist", true},
+        {"a_s_d_f_foobar.baz", false},
+        {"idontexist", false},
+    };
+
+    for (size_t n = 0; n < (sizeof(real_or_not) / sizeof(real_or_not[0])); n++) {
+        bool exists = false;
+        bool ret    = systest_pathexists(real_or_not[n].path, &exists);
+        all_passed &= ret;
+        if (!ret)
+            continue;            
+
+        if (exists != real_or_not[n].exists) {
+            all_passed = false;
+            printf("\t" RED("systest_pathexists('%s') = %s") "\n",
+                real_or_not[n].path, exists ? "true" : "false");            
+        } else {
+            printf("\t" GREEN("systest_pathexists('%s') = %s") "\n",
+                real_or_not[n].path, exists ? "true" : "false");               
+        }
+    }
     
     return all_passed;
 }
@@ -190,25 +222,6 @@ int main(int argc, char *argv[]) {
     ret = check_filesystem_api();
     handle_result(ret, "filesystem api");
 
-    //
-    // begin portability tests
-    //
-
-    /* file existence: some we know exist, and some we know don't. */
-    static const char* exist1   = "../../LICENSE";
-    static const char* exist2   = "./i_exist";
-    static const char* noexist1 = "a_s_d_f_foobar.baz";
-    static const char* noexist2 = "idontexist";
-
-    /* these log errors to the console if they are wrong, too. */
-    bool file_exist_result = true;
-    file_exist_result &= file_exists(exist1, true);
-    file_exist_result &= !file_exists(noexist1, false);
-    file_exist_result &= file_exists(exist2, true);
-    file_exist_result &= !file_exists(noexist2, false);
-
-    handle_result(file_exist_result, "file_exists");
-
     if (num_succeeded != num_attempted)
         printf("\t" WHITE("--- %d/%d tests passed ---\n"), num_succeeded, num_attempted);
     else
@@ -224,8 +237,236 @@ int main(int argc, char *argv[]) {
 // portability test implementations
 //
 
+bool systest_pathexists(const char* restrict path, bool* restrict exists) {
+    if (!_validstr(path) || !_validptr(exists))
+        return false;
 
-bool file_exists(const char* restrict path, bool really_exists) {
+#if !defined(_WIN32)
+    struct stat st = {0};
+    if (0 != stat(path, &st)) {
+        if (ENOENT == errno) {
+            *exists = false;
+            return true;
+        }
+        self_log("stat('%s') = %s", path, strerror(errno));
+        return false;
+    } else {
+        *exists = true;
+        self_log("'%s' exists, and is %ld bytes in size.", path,
+            (long)st.st_size);
+        return true;
+    }
+#else    
+   *exists = (TRUE == PathFileExistsA(path));
+#endif
+}
+
+char* systest_getcwd(void) {
+#if !defined(_WIN32)
+# if defined(__linux__) && defined(_GNU_SOURCE)
+    char* cur = get_current_dir_name();
+    if (NULL == cur)
+        handle_error(errno, "");
+    return cur;
+# else
+    char* cur = getcwd(NULL, 0);
+    if (NULL == cur)
+        handle_error(errno, "");
+    return cur;
+# endif
+#else // _WIN32
+    char* cur = _getcwd(NULL, 0);
+    if (NULL == cur)
+        handle_error(errno, "");
+    return cur;
+#endif
+}
+
+char* systest_getappfilename(void) {
+
+    char* buffer = (char*)calloc(SYSTEST_MAXPATH, sizeof(char));
+    if (NULL == buffer) {
+        handle_error(errno, "");
+        return NULL;
+    }
+
+    bool resolved = false;
+    size_t size   = SYSTEST_MAXPATH;
+    bool grow     = false;
+
+    do {
+        if (grow) {
+            self_log("reallocating %zu bytes for buffer and trying again...", size);
+            systest_safefree(buffer);
+
+            buffer = (char*)calloc(size, sizeof(char));
+            if (NULL == buffer) {
+                handle_error(errno, "");
+                resolved = false;
+                break;
+            }
+
+            grow = false;
+        }
+
+#if !defined(_WIN32)
+# if defined(__linux__)
+#  if defined(__HAVE_UNISTD_READLINK__)
+        ssize_t read = readlink("/proc/self/exe", buffer, size);
+        self_log("readlink() returned: %ld (size = %zu)", read, size);
+        if (-1 != read && read < (ssize_t)size) {
+            resolved = true;
+            break;
+        } else {
+            if (-1 == read) {
+                handle_error(errno, "");
+                resolved = false;
+                break;
+            } else if (read >= (ssize_t)size) {
+                /* readlink, like Windows' impl, doesn't have a concept
+                * of letting you know how much larger your buffer needs
+                * to be; it just truncates the string and returns how
+                * many chars it wrote there. */
+                size += SIR_PATH_BUFFER_GROW_BY;
+                grow = true;
+                continue;
+            }
+        }
+#  else
+#   error "unable to resolve readlink(); see man readlink and its feature test macro requirements."
+#  endif
+# elif defined(__FreeBSD__)
+        int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1 };
+        int ret = sysctl(mib, 4, buffer, &size, NULL, 0);
+        self_log("sysctl() returned: %d (size = %zu)", ret, size);
+        if (0 == ret) {
+            resolved = true;
+            break;
+        } else {
+            handle_error(errno, "");
+            resolved = false;
+            break;
+        }
+# elif defined(__APPLE__)
+        int ret = _NSGetExecutablePath(buffer, (uint32_t*)&size);
+        if (0 == ret) {
+            resolved = true;
+            break;
+        } else if (-1 == ret) {
+            grow = true;
+            continue;
+        } else {
+            handle_error(errno, "");
+            resolved = false;
+            break;
+        }
+# else
+#  error "no implementation for your platform; please contact the author."
+# endif
+#else // _WIN32
+        DWORD ret = GetModuleFileNameA(NULL, buffer, (DWORD)size);
+        self_log("GetModuleFileNameA() returned: %lu (size = %zu)", ret, size);
+        if (0 != ret && ret < (DWORD)size) {
+            resolved = true;
+            break;
+        } else {
+            if (0 == ret) {
+                handle_error(GetLastError(), "GetModuleFileNameA() failed!");
+                resolved = false;
+                break;
+            } else if (ret == (DWORD)size || ERROR_INSUFFICIENT_BUFFER == GetLastError()) {
+                /* Windows has no concept of letting you know how much larger
+                * your buffer needed to be; it just truncates the string and
+                * returns size. So, we'll guess. */
+                size += SIR_PATH_BUFFER_GROW_BY;
+                grow = true;
+                continue;
+            }
+        }
+#endif
+
+    } while (true);
+
+    if (!resolved) {
+        systest_safefree(buffer);
+        self_log("failed to resolve filename!");
+    }
+    else {
+        self_log("successfully resolved: '%s'", buffer);
+    }
+
+    return buffer;
+}
+
+char* systest_getappdir(void) {
+    char* filename = systest_getappfilename();
+    if (!_validstr(filename))
+        return NULL;
+
+    char* dirname = strdup(filename);
+    if (!_validstr(dirname)) {
+        systest_safefree(filename);
+        return NULL;
+    }
+
+    systest_safefree(filename);
+    return systest_getdirname(dirname);
+}
+
+char* systest_getbasename(char* restrict path) {
+    if (!_validstr(path))
+        return ".";
+
+#if !defined(_WIN32)
+    return basename(path);
+#else
+    // this isn't going to work; basename() and dirname() will strip
+    // path components off the end, too. need to verify whether this
+    // function only returns a file name part if it contains a full stop,
+    // or perhaps it queries the path to see if it's a file or directory.
+    // https://www.man7.org/linux/man-pages/man3/basename.3.html
+    return PathFindFileNameA(path);
+#endif
+}
+
+char* systest_getdirname(char* restrict path) {
+    if (!_validstr(path))
+        return ".";
+
+#pragma message("TODO: come back and revisit dirname_r")
+#if !defined(_WIN32)
+/*# if defined(__APPLE__)
+    //char dir[SYSTEST_MAXPATH] = {0};
+    char* retval = dirname_r(path, NULL);
+    return retval;
+# else*/
+    return dirname(path);
+//# endif
+#else
+    // TODO: same problem as above.
+    if (!PathRemoveFileSpecA((LPSTR)path))
+        handle_error(GetLastError(), "PathRemoveFileSpecA() failed!");
+    return path;
+#endif
+}
+
+bool systest_ispathrelative(const char* restrict path, bool* restrict relative) {
+    if (!_validstr(path) || !_validptr(relative))
+        return false;
+
+#if !defined(_WIN32)
+    if (path[0] == '/' || (path[0] == '~' && path[1] == '/'))
+        *relative = false;
+    else
+        *relative = true;
+    return true;
+#else
+    *relative = (TRUE == PathIsRelativeA(path));
+    return true;
+#endif    
+}
+
+/* bool file_exists(const char* restrict path, bool really_exists) {
 
     if (!path || !*path) {
         handle_error(EINVAL, "path is an invalid string");
@@ -252,7 +493,7 @@ bool file_exists(const char* restrict path, bool really_exists) {
 #endif
 
     if (retval != really_exists)
-        handle_problem("file: %s, really_exists: %s, errno: %s", path, bool_to_str(really_exists),
+        self_log("file: %s, really_exists: %s, errno: %s", path, bool_to_str(really_exists),
             strerror(errno));
 
     return retval;
@@ -282,7 +523,7 @@ bool systest_getcwd(char* restrict dir, size_t size) {
     return true;
 #   endif
 #else
-    /* _WIN32 */
+    /* _WIN32 * /
     if (NULL == _getcwd(dir, (int)size)) {
         handle_error(errno, "_getcwd() failed");
         return false;
@@ -333,7 +574,7 @@ bool systest_getappfilename(char* restrict buffer, size_t size) {
 #elif defined(__APPLE__)
     uint32_t size32 = (uint32_t)size;
     if (0 != _NSGetExecutablePath(buffer, &size32)) {
-        /* buffer is too small; need size32 bytes */
+        /* buffer is too small; need size32 bytes * /
         handle_error(ENOBUFS, "_NSGetExecutablePath() failed (buffer too small)!");
         retval = false;
     } else {
@@ -342,7 +583,7 @@ bool systest_getappfilename(char* restrict buffer, size_t size) {
 #else
 #   error "no implementation for your platform; please contact the author."
 #endif
-#else /* _WIN32 */
+#else /* _WIN32 * /
     if (0 == GetModuleFileNameA(NULL, buffer, (DWORD)size)) {
         handle_error(GetLastError(), "GetModuleFileNameA() failed");
         retval = false;
@@ -397,7 +638,7 @@ char* systest_getdirname(char* path) {
         handle_error(GetLastError(), "PathRemoveFileSpecA() failed!");
     return path;
 #endif
-}
+} */
 
 
 //
@@ -406,10 +647,9 @@ char* systest_getdirname(char* path) {
 
 
 void _handle_error(int err, const char* msg, char* file, int line, const char* func) {
-    fprintf(stderr, RED("ERROR: %s:%d in %s: error = '%s' (%s)") "\n", systest_getbasename(file), line, func, strerror(err), msg);
+    fprintf(stderr, RED("ERROR: %s:%d in %s: error = '%s' (%s)") "\n", file, line, func, strerror(err), msg);
 }
 
-void _handle_problem(const char* msg, char* file, int line, const char* func) {
-    fprintf(stderr, RED("ERROR: %s:%d in %s: %s") "\n", systest_getbasename(file), line, func, msg);
+void _self_log(const char* msg, char* file, int line, const char* func) {
+    fprintf(stderr, WHITE("%s:%d in %s: %s") "\n", file, line, func, msg);
 }
-
--- support/systest.h
+++ support/systest.h
@@ -2,59 +2,56 @@
 #define _SYSTEST_H_INCLUDED
 
 #if !defined(_WIN32)
-#   if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
-#       define __BSD__
-#       define _BSD_SOURCE
-#   endif
+# if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+#  define __BSD__
+#  define _BSD_SOURCE
+# endif
 
-#   define _DEFAULT_SOURCE
-#   define _GNU_SOURCE
-#   define _POSIX_C_SOURCE 200809L
-#   define _XOPEN_SOURCE 700
-#   define __STDC_WANT_LIB_EXT1__ 1
+#define _DEFAULT_SOURCE
+#define _GNU_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#define _XOPEN_SOURCE 700
+#define __STDC_WANT_LIB_EXT1__ 1
 
-#   include <sys/types.h>
-#   include <sys/stat.h>
-#   include <unistd.h>
-#   include <libgen.h>
-#   include <limits.h>
-#   include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <limits.h>
+#include <fcntl.h>
 
-#   if defined(__GLIBC__)
-#       if (__GLIBC__ >= 2 && __GLIBC_MINOR__ > 19)  || \
-           (__GLIBC__ == 2 && __GLIBC_MINOR__ <= 19) && defined(_BSD_SOURCE)
-#           define __HAVE_UNISTD_READLINK__
-#       endif
-#   endif
+# if defined(__GLIBC__)
+#  if (__GLIBC__ >= 2 && __GLIBC_MINOR__ > 19)  || \
+      (__GLIBC__ == 2 && __GLIBC_MINOR__ <= 19) && defined(_BSD_SOURCE)
+#   define __HAVE_UNISTD_READLINK__
+#  endif
+# endif
 
-#   if defined(PATH_MAX)
-#       define SYSTEST_MAXPATH PATH_MAX
-#   else
-#       define SYSTEST_MAXPATH 1024
-#   endif
+# if defined(PATH_MAX)
+#  define SYSTEST_MAXPATH PATH_MAX
+# else
+#  define SYSTEST_MAXPATH 1024
+# endif
+#else // _WIN32
+# define __WANT_STDC_SECURE_LIB__ 1
+# define WIN32_LEAN_AND_MEAN
+# define WINVER       0x0A00
+# define _WIN32_WINNT 0x0A00
+# include <windows.h>
+# include <shlwapi.h>
+# include <pathcch.h>
+# include <direct.h>
 
-#   define STRFMT(clr, s) clr s "\033[0m"
-#   define RED(s) STRFMT("\033[1;91m", s)
-#   define GREEN(s) STRFMT("\033[1;92m", s)
-#   define WHITE(s) STRFMT("\033[1;97m", s)
-#   define BLUE(s) STRFMT("\033[1;34m", s)
-#else
-#   define __WANT_STDC_SECURE_LIB__ 1
-#   define WIN32_LEAN_AND_MEAN
-#   define WINVER       0x0A00
-#   define _WIN32_WINNT 0x0A00
-#   include <windows.h>
-#   include <shlwapi.h>
-#   include <pathcch.h>
-#   include <direct.h>
-
-#   define SYSTEST_MAXPATH MAX_PATH
-
-#   define RED(s) s
-#   define GREEN(s) s
-#   define WHITE(s) s
-#   define BLUE(s) s
+# define SYSTEST_MAXPATH MAX_PATH
 #endif
+
+#define STRFMT(clr, s) clr s "\033[0m"
+#define RED(s) STRFMT("\033[1;91m", s)
+#define GREEN(s) STRFMT("\033[1;92m", s)
+#define WHITE(s) STRFMT("\033[1;97m", s)
+#define BLUE(s) STRFMT("\033[1;34m", s)
+#define CYAN(s) STRFMT("\033[1;36m", s)
+#define YELLOW(s) STRFMT("\033[1;33m", s)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -79,32 +76,90 @@ extern "C" {
 //
 // portability test implementations
 //
-bool file_exists(const char* restrict path, bool really_exists);
 
-bool systest_getcwd(char* restrict buffer, size_t size);
-bool systest_getappfilename(char* restrict buffer, size_t size);
-bool systest_getappdir(char* restrict buffer, size_t size);
+
+/** Defines how many characters to grow a buffer by which was deemed too small
+ * by a system call (with no information regarding the necessary size). */
+#define SYSTEST_PATH_BUFFER_GROW_BY 32
+
+/**
+ * Determines if a file or directory exists in the filesystem.
+ * Returns false if an error occurs or an argument is invalid. Places the result
+ * in \p exists.
+*/
+bool systest_pathexists(const char* restrict path, bool* restrict exists);
+
+/**
+ * Returns the current working directory for the calling process. If an error
+ * occurs, returns NULL. Pointers returned must be deallocated with free().
+ */
+char* systest_getcwd(void);
+
+/**
+ * Returns the absolute path of the binary executable file for the calling
+ * process. If an error occurs, returns NULL. Pointers returned must be
+ * deallocated with free().
+ */
+char* systest_getappfilename(void);
+
+/**
+ * Returns the absolute path of the directory containing the binary file
+ * of the calling process (not necessarily the current working directory).
+ */
+char* systest_getappdir(void);
+
+/**
+ * Returns only last component of a path.
+ * May return ".", "/", or \p path if no determination can be made.
+ */
 char* systest_getbasename(char* restrict path);
-char* systest_getdirname(char* path); 
+
+/**
+ * Returns all but the last component of a path.
+ * May return "." "/", or \p path if no determination can be made.
+*/
+char* systest_getdirname(char* restrict path);
+
+/**
+ * Determines if a given path is relative (or absolute).
+ * Returns false if an error occurs or an argument is invalid. Places the result
+ * in \p relative.
+ */
+bool systest_ispathrelative(const char* restrict path, bool* restrict relative);
 
 //
 // utility functions
 //
 
 /* this is strictly for use when encountering an actual failure of a system call. 
- * use handle_problem to report things other than error numbers. */
+ * use self_log to report things other than error numbers. */
 void _handle_error(int err, const char* msg, char* file, int line, const char* func);
 #define handle_error(err, msg) _handle_error(err, msg, __FILE__, __LINE__, __func__);
 
-void _handle_problem(const char* msg, char* file, int line, const char* func);
+void _self_log(const char* msg, char* file, int line, const char* func);
 
-static char _handle_problem_buf[512] = {0};
-#define handle_problem(problem, ...)  \
-    snprintf(_handle_problem_buf, 512, problem, __VA_ARGS__); \
-    _handle_problem(_handle_problem_buf, __FILE__, __LINE__, __func__);
+static char _self_log_buf[512] = {0};
+#define self_log(...)  \
+    snprintf(_self_log_buf, 512, __VA_ARGS__); \
+    _self_log(_self_log_buf, __FILE__, __LINE__, __func__);
 
-/* converts bool -> const char* */
-#define bool_to_str(b) b ? "true" : "false"
+#define bool_to_str(b) (b ? "true" : "false")
+#define prn_str(str) (str ? str : "NULL")
+#define _validstr(str) (NULL != str && '\0' != *str)
+#define _validptr(p) (NULL != p)
+
+static inline
+void _systest_safefree(void** p) {
+    if (!p || (p && !*p))
+        return;
+    free(*p);
+    *p = NULL;
+}
+
+static inline
+void systest_safefree(void* p) {
+    _systest_safefree(&p);
+}
 
 #ifdef __cplusplus
 }
